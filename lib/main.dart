b7b96133e4652fda24cf77fa9dce856bde2320b0
chanwoo7 <chanwoo9987@gmail.com>
2025-01-20 13:28:16 +0900

    Part 1-4: 플러터 기본 앱 레이아웃 (Scaffold)

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      // 글자
-      // home: Text('안녕'),
+      home: Scaffold(
+        /**
+         * 상단바 / body / 하단바
+         */
+        // appBar: AppBar( title: Text('앱제목') ),
+        // body: Text('안녕'), // body는 필수
+        // bottomNavigationBar: BottomAppBar( child: Text('하단바') ),
 
-      // 아이콘
-      // home: Icon(Icon.star),
 
-      // 이미지
-      // home: Image.asset('assets/dog.png'),
+        /**
+         * 가로 / 세로로 위젯 배치
+         */
+        // body: Row (  // Column 쓰면 세로 배치
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //     // spaceEvenly: 모든 여백 동일
+        //     // spaceBetween: 양 끝에 우선 배치
+        //     // spaceAround: 모든 여백 동일한데 양 끝 여백은 절반만큼
+        //     // start: 시작 부분에 다 모여
+        //     // end: 끝 부분에 다 모여
+        //     // center: 중간에 다 모여
+        //
+        //   children: [ Icon(Icons.star), Icon(Icons.star), Icon(Icons.star) ]
+        // ),
 
-      // 네모 박스 + width, height 설정
-      // home: Center(
-      //     child: Container(width : 50, height : 50, color: Colors.blue)
-      // ),
 
-      // 위젯 안에 위젯
-      home: Container(
-        child: Text('박스 안 글자임'),
-      )
+        /**
+         * 세로 / 가로 배치 간격 조절
+         */
+        body: Container (
+          color: Colors.grey,
+          height : 400,  // 높이가 있어야 세로 정렬 가능
+          child : Row (
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,  // mainAxisAlignment
+            // Row -> mainAxisAlignment는 가로, crossAxis는 세로
+            // Column -> mainAxisAlignment는 세로, crossAxis는 가로
+
+            children: const [
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+            ],
+          ),
+        ),
+
+
+        /**
+         * 숙제: 하단바 디자인
+         */
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            // height: 60,  // 왜 안 동작하지?
+            child: Row (
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)
+              ],
+            )
+          )
+        ),
+
+      ),
     );
   }
 }
